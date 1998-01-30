70803aabed36f5cbc18770a456266bb12e7a5e33
danwang <>
1998-01-30 18:22:11 +0000

    Attempting to build automatically on NT

--- asdlGen/src/misc/find_runtime.sh
+++ asdlGen/src/misc/find_runtime.sh
@@ -1,5 +1,14 @@
 #! /bin/sh
 # Guess the location of the sml runtime system 
-prog=`which sml`
-runtime=`sh -x $prog 2>&1 < /dev/null | awk '/.*exec/ { print $3;}' `
-echo $runtime
+prog=`which sml-cm`
+case `basename $prog` in
+ sml-cm.bat)
+   #icck don't ask...
+    eval "smldir=$(awk -F = '/@SET SMLNJ_HOME/ {print $2}' $prog|tr '\\' '/')"
+    runtime=`echo ${smldir}/bin/.run/run.x86-win32.exe | cut -c3-`
+ ;;
+ *) runtime=`sh -x $prog 2>&1 < /dev/null | awk '/.*exec/ { print $3;}' ` 
+ ;;
+esac
+name=`basename ${runtime}`
+echo $runtime $name 
